77dadfa2b70e399a752321ac5098a0dd6da11997
Onyemaechi Okafor <onyemaechiokafor@onyemaechis-mbp.lan>
2024-09-03 10:04:02 +0100

    add package web

--- lib/browser_adapter.dart
+++ lib/browser_adapter.dart
@@ -1,5 +1,5 @@
 library browser_adapter;
 
 export 'unsupported.dart'
-    if (dart.library.html) 'html.dart'
+    if (dart.library.js_interop) 'html.dart'
     if (dart.library.io) 'io.dart';
--- lib/html.dart
+++ lib/html.dart
@@ -1,23 +1,15 @@
 @JS()
 
 import 'dart:async';
-
+import 'dart:js_interop' as interop;
+import 'dart:ui_web' as ui_web;
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
+import 'package:web/web.dart' as web;
 import 'interface.dart' as inter;
 
 import 'package:js/js.dart';
-
-/// A JavaScript entrypoint that allows developer to set rendering backend
-/// at runtime before launching the application.
-
-@JS('window.flutterWebRenderer')
-external String get requestedRendererType;
-
-@JS('window.isSafariBrowserLike')
-external bool get isSafariBrowserLike;
 
 @JS('window.browserVersion')
 external String get browserVersion;
@@ -25,139 +17,46 @@ external String get browserVersion;
 @JS('navigator.standalone')
 external bool? get standAlone;
 
-const bool _autoDetect =
-    bool.fromEnvironment('FLUTTER_WEB_AUTO_DETECT', defaultValue: false);
+bool get isWasm => ui_web.BrowserDetection.instance.isWasm;
 
-const bool _useSkia =
-    bool.fromEnvironment('FLUTTER_WEB_USE_SKIA', defaultValue: false);
+bool get isDesktopBrowser => ui_web.BrowserDetection.instance.isDesktop;
 
-enum OperatingSystem {
-  /// iOS: <http://www.apple.com/ios/>
-  iOs,
+bool get isPWA =>
+    (standAlone ?? false) ||
+    web.window.matchMedia('(display-mode: standalone)').matches;
 
-  /// Android: <https://www.android.com/>
-  android,
+bool get isSafariBrowser => ui_web.BrowserDetection.instance.isSafari;
 
-  /// Linux: <https://www.linux.org/>
-  linux,
-
-  /// Windows: <https://www.microsoft.com/windows/>
-  windows,
-
-  /// MacOs: <https://www.apple.com/macos/>
-  macOs,
-
-  /// We were unable to detect the current operating system.
-  unknown,
-}
-
-const Set<OperatingSystem> _desktopOperatingSystems = {
-  OperatingSystem.macOs,
-  OperatingSystem.linux,
-  OperatingSystem.windows,
-};
-
-const Set<OperatingSystem> _mobileOperatingSystems = {
-  OperatingSystem.iOs,
-  OperatingSystem.android,
-};
-
-class WebBrowserDetectorImp {
-  WebBrowserDetectorImp() {
-    _operatingSystem = _detectOperatingSystem();
-  }
-
-  OperatingSystem? _operatingSystem;
-
-  OperatingSystem _detectOperatingSystem() {
-    final String platform = html.window.navigator.platform!;
-    final String userAgent = html.window.navigator.userAgent;
-
-    if (platform.startsWith('Mac')) {
-      return OperatingSystem.macOs;
-    } else if (platform.toLowerCase().contains('iphone') ||
-        platform.toLowerCase().contains('ipad') ||
-        platform.toLowerCase().contains('ipod')) {
-      return OperatingSystem.iOs;
-    } else if (userAgent.contains('Android')) {
-      // The Android OS reports itself as "Linux armv8l" in
-      // [html.window.navigator.platform]. So we have to check the user-agent to
-      // determine if the OS is Android or not.
-      return OperatingSystem.android;
-    } else if (platform.startsWith('Linux')) {
-      return OperatingSystem.linux;
-    } else if (platform.startsWith('Win')) {
-      return OperatingSystem.windows;
-    } else {
-      return OperatingSystem.unknown;
-    }
-  }
-
-  bool get isDesktopBrowser {
-    return _desktopOperatingSystems.contains(_operatingSystem);
-  }
-
-  bool get isaMobileSafari => _operatingSystem == OperatingSystem.iOs;
-
-  bool get isaMobile => _mobileOperatingSystems.contains(_operatingSystem);
-}
-
-bool isCanvasKitRenderer() {
-  bool isCanvaskit = requestedRendererType == 'canvaskit';
-  if (isCanvaskit) {
-    return isCanvaskit;
-  }
-  return _autoDetect ? WebBrowserDetectorImp().isDesktopBrowser : _useSkia;
-}
-
-bool isDesktopBrowser() {
-  return WebBrowserDetectorImp().isDesktopBrowser;
-}
-
-bool isPWA() {
-  return (standAlone ?? false) ||
-      html.window.matchMedia('(display-mode: standalone)').matches;
-}
-
-bool isClipSupported() {
-  if (isSafariBrowserLike && isCanvasKitRenderer()) {
-    return !isSafariBrowserLike;
-  }
-  return true;
-}
-
-bool isSafariBrowser() {
-  return isSafariBrowserLike;
-}
-
-bool isMobileBrowser() {
-  return WebBrowserDetectorImp().isaMobile;
-}
+bool get isMobileBrowser => ui_web.BrowserDetection.instance.isMobile;
 
 void replaceBrowserUrl(String path, {String title = ''}) {
-  final currentState = html.window.history.state;
-  html.window.history.replaceState(currentState, title, path);
+  final currentState = web.window.history.state;
+  web.window.history.replaceState(currentState, title, path);
 }
 
 EdgeInsets getExtraPadding() {
   //if (!isDesktopBrowser())
-  final sat = html.document.documentElement
-          ?.getComputedStyle()
-          .getPropertyValue("--sat") ??
+  final sat = web.document.documentElement
+          ?.computedStyleMap()
+          .get("--sat")
+          .toString() ??
       '';
-  final sar = html.document.documentElement
-          ?.getComputedStyle()
-          .getPropertyValue("--sar") ??
-      '';
-
-  final sab = html.document.documentElement
-          ?.getComputedStyle()
-          .getPropertyValue("--sab") ??
+  final sar = web.document.documentElement
+          ?.computedStyleMap()
+          .get("--sar")
+          .toString() ??
       '';
 
-  final sal = html.document.documentElement
-          ?.getComputedStyle()
-          .getPropertyValue("--sal") ??
+  final sab = web.document.documentElement
+          ?.computedStyleMap()
+          .get("--sab")
+          .toString() ??
+      '';
+
+  final sal = web.document.documentElement
+          ?.computedStyleMap()
+          .get("--sal")
+          .toString() ??
       '';
   //print('sat: $sat, sar:$sar, sab:$sab, sal:$sal');
   return EdgeInsets.fromLTRB(
@@ -172,16 +71,17 @@ double stringToVal(String val) {
 }
 
 void initializeViewPort() {
-  final viewportMeta = html.MetaElement()
-    ..setAttribute('flt-viewport', '')
-    ..name = 'viewport'
-    ..content =
-        'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover';
-  html.document.head!.append(viewportMeta);
+  web.HTMLElement.nav();
+  final viewportMeta = web.document.createElement('meta')
+    ..setAttribute('flt-viewport', '');
+  viewportMeta.setAttribute('name', 'viewport');
+  viewportMeta.setAttribute('content',
+      'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover');
+  web.document.head!.append(viewportMeta);
 }
 
 void removeDocument(String id) {
-  final loader = html.document.getElementById(id);
+  final loader = web.document.getElementById(id);
   if (loader != null) {
     loader.remove();
   }
@@ -191,12 +91,12 @@ class FocusOutDetector extends inter.FocusOutDetector {
   VoidCallback? onFocusOut;
 
   FocusOutDetector({this.onFocusOut}) {
-    html.document.addEventListener('focusout', _handleFocusOut);
+    web.document.addEventListener('focusout', _handleFocusOut.toJS);
   }
 
   @override
   dispose() {
-    html.document.removeEventListener('focusout', _handleFocusOut);
+    web.document.removeEventListener('focusout', _handleFocusOut.toJS);
   }
 
   void _handleFocusOut(event) {
@@ -208,7 +108,7 @@ class PageVisibilityDetector extends inter.PageVisibilityDetector {
   ValueChanged<bool>? onVisibilityChanged;
   late StreamSubscription sub;
   PageVisibilityDetector({this.onVisibilityChanged}) {
-    sub = html.document.onVisibilityChange.listen(_handleChange);
+    sub = web.document.onVisibilityChange.listen(_handleChange);
   }
 
   @override
@@ -217,7 +117,7 @@ class PageVisibilityDetector extends inter.PageVisibilityDetector {
   }
 
   void _handleChange(event) {
-    final visible = !(html.document.hidden ?? false);
+    final visible = !(web.document.hidden);
     onVisibilityChanged?.call(visible);
   }
 }
@@ -226,7 +126,7 @@ class OnFirstFrameListener extends inter.PageVisibilityDetector {
   VoidCallback? onFirstFrame;
 
   OnFirstFrameListener({this.onFirstFrame}) {
-    html.window.addEventListener('flutter-first-frame', _handleFirstFrame);
+    web.window.addEventListener('flutter-first-frame', _handleFirstFrame.toJS);
   }
 
   void _handleFirstFrame(event) {
@@ -235,7 +135,8 @@ class OnFirstFrameListener extends inter.PageVisibilityDetector {
 
   @override
   dispose() {
-    html.window.removeEventListener('flutter-first-frame', _handleFirstFrame);
+    web.window
+        .removeEventListener('flutter-first-frame', _handleFirstFrame.toJS);
   }
 }
 
@@ -243,33 +144,31 @@ const _kHeuristicViewPortHeightClientRatio = 0.85;
 
 class KeyboardHeightVisibilityDetector
     extends inter.KeyboardHeightVisibilityDetector {
-  late Stream<bool> _stream;
+  final BehaviorSubject<bool> _sub = BehaviorSubject();
   KeyboardHeightVisibilityDetector() {
     if (!isSupported()) {
       return;
     }
-    _stream = MergeStream([
-      html.window.visualViewport!.onResize
-          .map<html.VisualViewport>((_) => html.window.visualViewport!),
-      html.window.visualViewport!.onScroll
-          .map<html.VisualViewport>((_) => html.window.visualViewport!),
-      html.window.onScroll
-          .map<html.VisualViewport>((_) => html.window.visualViewport!),
-    ]).map<bool>((event) {
-      if (html.document.documentElement?.clientHeight != null &&
-          event.height != null &&
-          event.scale != null) {
-        return ((event.height! * event.scale!) /
-                html.document.documentElement!.clientHeight) <
-            _kHeuristicViewPortHeightClientRatio;
-      }
-      return false;
-    }).distinct();
+    web.window.visualViewport!.onscroll = _checkViewPort.toJS;
+    web.window.visualViewport!.onscroll = _checkViewPort.toJS;
+  }
+
+  void _checkViewPort(event) {
+    final viewPort = web.window.visualViewport;
+    bool keyBoardVisible = false;
+    if (web.document.documentElement?.clientHeight != null &&
+        viewPort?.height != null &&
+        viewPort?.scale != null) {
+      keyBoardVisible = ((viewPort!.height * viewPort.scale) /
+              web.document.documentElement!.clientHeight) <
+          _kHeuristicViewPortHeightClientRatio;
+    }
+    _sub.add(keyBoardVisible);
   }
 
   @override
-  Stream<bool> getStream() => _stream;
+  Stream<bool> getStream() => _sub.stream.distinct();
 
   @override
-  bool isSupported() => html.window.visualViewport != null;
+  bool isSupported() => web.window.visualViewport != null;
 }
